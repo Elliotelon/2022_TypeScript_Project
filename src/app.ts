307a2f699b54a5f47c08c56aabff18faccf68351
kimminkyu <kmk50009@naver.com>
2022-02-18 00:04:07 +0900

    Add ProjectList Class

--- src/app.ts
+++ src/app.ts
@@ -62,6 +62,38 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjDescriptor;
 }
 
+// ProjectList Class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
 // ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -103,25 +135,25 @@ class ProjectInput {
     const enteredPeople = this.peopleInputElement.value;
 
     const titleValidatable: Validatable = {
-        value: enteredTitle,
-        required: true
-    }
+      value: enteredTitle,
+      required: true,
+    };
     const descriptionValidatable: Validatable = {
-        value: enteredDescription,
-        required: true,
-        minLength: 5
-    }
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
     const peopleValidatable: Validatable = {
-        value: +enteredPeople,
-        required: true,
-        min: 1,
-        max: 5
-    }
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
 
     if (
-        !validate(titleValidatable) ||
-        !validate(descriptionValidatable) ||
-        !validate(peopleValidatable)
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
       //enteredTitle.trim().length === 0 ||
       //enteredDescription.trim().length === 0 ||
       //enteredPeople.trim().length === 0
@@ -160,3 +192,6 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
+
